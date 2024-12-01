b286cbb57815b57ee363fd1ba9e976fdd7b0da5e
ivanHuerta14 <146044360+ivanHuerta14@users.noreply.github.com>
2024-11-30 21:11:17 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ let sortOrder = 'ASC';
 
 function loadTableData() {
     searchQuery = $('#searchInput').val() || '';
-    
+    const url = `https://proyecto2-production-80d3.up.railway.app/api.php?action=read&search=${searchQuery}`;
     $.ajax({
-        url: `api.php?action=read&search=${searchQuery}&column=${sortColumn}&order=${sortOrder}&page=${currentPage}`,
+       // url: `api.php?action=read&search=${searchQuery}&column=${sortColumn}&order=${sortOrder}&page=${currentPage}`,
+        url: url,
         method: 'GET',
         dataType: 'json',
         success: function(data) {
